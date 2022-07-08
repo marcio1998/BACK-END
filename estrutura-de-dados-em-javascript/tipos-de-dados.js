f4facbcee9a6231ba6ca17657327d68db25a1b65
Márcio Gabriel Mengali <marciogabriel1998@gmail.com>
2022-07-07 22:54:01 -0300

    estutura-de-dados-em-javascript-intial Commit

---
+++ estrutura-de-dados-em-javascript/tipos-de-dados.js
@@ -0,0 +1,12 @@
+const somosCasados = true;
+const anoNascimento = 1998;
+const media = (7 + 8 + 9 + 10) / 4;
+let idade = null;
+const terceiroFilho = undefined;
+const numeroGrande = 22555555444n;
+const numeroGrande2 = BigInt('25555558888777n');
+const meuSimbolo = new Symbol();
+const meuObjeto = {
+  curso: 'Estrutura de Dadoss',
+  instrutuores: ['Márcio', 'Gabriel'],
+};
