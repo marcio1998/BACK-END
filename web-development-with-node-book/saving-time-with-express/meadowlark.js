583640f621d4368bb6a0ef90f7f26a12d1fc9700
marcio1998 <marciogabriel1998@gmail.com>
2022-07-13 22:12:09 -0300

    saving-time-with-express-headers-route

--- web-development-with-node-book/saving-time-with-express/meadowlark.js
+++ web-development-with-node-book/saving-time-with-express/meadowlark.js
@@ -48,6 +48,15 @@ app.get('/tours/request-group-rate', function(req, res){
     res.render('tours/request-group-rate');
 });
 
+//headers
+app.get('/headers', function(req, res){
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (let name in req.headers) s+= name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
+
+
 //static files
 app.use(express.static(__dirname + '/public'))
 
